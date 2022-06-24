ab937a4f49dee41eaaa96c512ee41653b4a70071
Todd James <todd@itodd.co>
2022-06-24 10:58:53 -0400

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Todd James on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
